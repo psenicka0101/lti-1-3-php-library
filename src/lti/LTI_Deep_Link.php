8e86cc8be54a60f2bf5e33e17d233f1447d5f93e
David Psenicka <dpsenick@gmail.com>
2021-09-14 11:13:15 -0500

    deep linking fix

--- src/lti/LTI_Deep_Link.php
+++ src/lti/LTI_Deep_Link.php
@@ -25,7 +25,8 @@ class LTI_Deep_Link {
             "https://purl.imsglobal.org/spec/lti/claim/message_type" => "LtiDeepLinkingResponse",
             "https://purl.imsglobal.org/spec/lti/claim/version" => "1.3.0",
             "https://purl.imsglobal.org/spec/lti-dl/claim/content_items" => array_map(function($resource) { return $resource->to_array(); }, $resources),
-            "https://purl.imsglobal.org/spec/lti-dl/claim/data" => $this->deep_link_settings['data'],
+            // "https://purl.imsglobal.org/spec/lti-dl/claim/data" => $this->deep_link_settings['data'],
+            "https://purl.imsglobal.org/spec/lti-dl/claim/data" => $this->deep_link_settings,
         ];
         return JWT::encode($message_jwt, $this->registration->get_tool_private_key(), 'RS256', $this->registration->get_kid());
     }
@@ -35,7 +36,8 @@ class LTI_Deep_Link {
         ?>
         <form id="auto_submit" action="<?= $this->deep_link_settings['deep_link_return_url']; ?>" method="POST">
             <input type="hidden" name="JWT" value="<?= $jwt ?>" />
-            <input type="submit" name="Go" />
+            <!-- <input type="submit" name="Go" /> -->
+            <div>Submitting configuration...</div>
         </form>
         <script>
             document.getElementById('auto_submit').submit();
